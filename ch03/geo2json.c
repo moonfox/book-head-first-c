9d38c4520dffbb68619f0d6340690ad04ca5b8a6
moonfox <sleepmoon.jia@gmail.com>
2019-06-20 15:21:49 +0800

    加入对错误坐标的判断

--- ch03/geo2json.c
+++ ch03/geo2json.c
@@ -17,6 +17,18 @@ int main(int argc, char const *argv[])
     else
       started = 1;
 
+    if ((lati < -90.0) || (lati > 90.0))
+    {
+      fprintf(stderr, "invalid latitude: %f\n", lati);
+      return 2;
+    }
+
+    if ((longi < -180.0) || (longi > 180.0))
+    {
+      fprintf(stderr, "invalid longitude: %f\n", longi);
+      return 2;
+    }
+
     printf("{latitude: %f, longitude: %f, info: '%s'}", lati, longi, info);
   }
 
